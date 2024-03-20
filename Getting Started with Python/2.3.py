1cea4b24cb171043d62d371987e56b21a55db40d
Emir Montazeri <137637411+EmirMontazeri@users.noreply.github.com>
2024-03-20 17:13:11 +0330

    Add files via upload

---
+++ Getting Started with Python/2.3.py
@@ -0,0 +1,7 @@
+# A program that asks for hours and pay per hours to calculate gross pay.
+hours = input('Enter Work Hours: ')
+pph = input('Enter Pay / Hours: ')
+hf = float(hours)
+pf = float(pph)
+gpay = hf * pf
+print('Gross Pay:', gpay)
